cba2193484520f0b57cdd3a1c25ac01481a747f1
AnthonyArmatas <armatasanthony@gmail.com>
2017-09-10 16:40:50 -0400

    This is the first commit to the re

---
+++ Proto-2d-Fighter/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+	}
+}
---
+++ Proto-2d-Fighter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode throwIce;
+
+
+    private Rigidbody2D theRB;
+    private Animator tor;
+    private Animation tion;
+
+	// Use this for initialization
+	void Start () {
+        theRB = GetComponent<Rigidbody2D>();
+        tor = GetComponent<Animator>();
+        tion = GetComponent<Animation>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(left))
+        {
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+            transform.localRotation = Quaternion.Euler(180, 0, 180);
+            tor.SetBool("Walking", true);
+            //tion.Stop("subZero_idle");
+            //tion.Play("subZero_walking");
+           // ani.SetBool("subZero_walking", true);
+        }
+        else if (Input.GetKey(right))
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            tor.SetBool("Walking", true);
+            //tion.Stop("subZero_idle");
+            //tion.Play("subZero_walking");
+            //ani.SetBool("subZero_walking", true);
+        }
+        else
+        {
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+            tor.SetBool("Walking", false);
+            //tion.Stop("subZero_walking");
+            //tion.Play("subZero_idle");
+
+        }
+
+        if (Input.GetKeyDown(jump))
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+	}
+}
