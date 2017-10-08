0c1d53fea2de4accc8822fff9014d83c4198eae4
AnthonyArmatas <armatasanthony@gmail.com>
2017-10-08 12:28:25 -0400

    Added images to character select screen. Need to have them autofixed in the script, and then add player selection, and transfering data to level selection screen. Also need to add player selection frames

---
+++ Proto-2d-Fighter/Assets/Scripts/CharacterPlacementScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterPlacementScript : MonoBehaviour {
+
+    //private GameObject;
+    CharacterSelectScript CSS;
+    GameObject charPanel;
+    private SpriteRenderer spriteImg;
+    
+	// Use this for initialization
+	void Start () {
+        charPanel = gameObject;//.transform.parent.gameObject.name
+        charPanel = gameObject.transform.parent.gameObject.transform.parent.gameObject;
+        //Debug.Log("Currents name " + charPanel.name);
+        CSS = charPanel.GetComponent<CharacterSelectScript>();
+        //Debug.Log("CharacterPlacement script it in " + gameObject.name);
+        //Debug.Log("This is CSS " + CSS);
+        spriteImg = gameObject.GetComponent<SpriteRenderer>();//.sprite = CSS.spriteList[0];
+        charPanel = gameObject;
+        //Debug.Log("Currents name " + charPanel.name);
+        for(int i = 0; i < CSS.spriteList.Length;i++){
+            if (charPanel.name.Equals("Character" + (i+1)))
+            {
+                spriteImg.sprite = CSS.spriteList[i];
+            }
+        }
+
+        //spriteImg.sprite = CSS.spriteList[1];
+
+ 	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Proto-2d-Fighter/Assets/Scripts/CharacterSelectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+
+
+public class CharacterSelectScript : MonoBehaviour {
+
+    public Sprite[] spriteList;
+    
+
+	// Use this for initialization
+	void Start () {
+       // Debug.Log(spriteList[0]);
+       // Debug.Log(spriteList[1]);
+       // Debug.Log(spriteList[2]);
+       // Debug.Log(spriteList[3]);
+       // Debug.Log(gameObject.name);
+       // Debug.Log("CharacterSelectscript it in " + gameObject.name);
+	}
+	
+}
