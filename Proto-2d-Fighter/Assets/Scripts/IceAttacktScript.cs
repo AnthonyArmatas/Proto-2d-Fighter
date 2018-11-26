86a0abaeb5abc572512305295612f2e601531819
Anthony Armatas <armatasanthony@gmail.com>
2018-11-26 10:31:08 -0800

    I re-added the basic attack functionality to the ice character to better work in an OOP schema. I created the Node class and am currently work on an algorithm to have it move around in a fun way to chase

--- Proto-2d-Fighter/Assets/Scripts/IceAttacktScript.cs
+++ Proto-2d-Fighter/Assets/Scripts/IceAttacktScript.cs
@@ -2,19 +2,30 @@
 
 
 public class IceAttacktScript : MonoBehaviour {
+
+    public KeyCode attack;
+
     private float nextFire;
 
     public float IceBFireRate = .5f; //.5f need f to make it a float
     public Transform IceBSpwnP; //Ironically, the attack's position needs to be the attackspawn's transforms's position 
     public Transform IceBSpwnR; //While the rotaion needs to be the aimpositions transformations rotaion
 
+    public Animator tor;
     public GameObject iceBolt;
 
     void Start()
     {
         //iceBolt = GetComponent<IceAttacktScript>();
+        tor = GetComponent<Animator>();
     }
-
+    void Update()
+    {
+        if (Input.GetKey(attack))
+        {
+            launchIceBolt(tor);
+        }
+    }
 
 
     public void launchIceBolt(Animator tor)
